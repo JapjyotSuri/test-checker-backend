4a9ad58bad72453a5f27823de4a385ee520ee607
Nipun2004 <nipunbansal546@gmail.com>
2026-06-07 15:11:33 +0530

    file size limit set to 5 MB

--- src/app.js
+++ src/app.js
@@ -30,8 +30,8 @@ app.use(cors({
   credentials: true
 }));
 app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 
 // Serve uploaded files
 // Serve uploaded files. Allow embedding these files in an iframe from the local frontend
